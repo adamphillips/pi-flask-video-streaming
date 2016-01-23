d79bc2e409a8ec0361adc9e709b73199677cfc7e
Adam Phillips <adam@29ways.co.uk>
2016-01-23 00:55:41 +0000

    Switched back to the face processor

--- app/camera_pi.py
+++ app/camera_pi.py
@@ -2,7 +2,7 @@ import time
 import io
 import threading
 import picamera
-from histogram_image_processor import ImageProcessor as ip
+from face_image_processor import ImageProcessor as ip
 import pdb
 
 class Camera(object):
