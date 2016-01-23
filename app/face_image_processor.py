02df1bb268004f80d74823c70a92c40796eb20d4
Adam Phillips <adam@29ways.co.uk>
2016-01-23 01:19:07 +0000

    Only calculate image sizes once
    A further optimisation to reduce the time take to process an image. This
    brings it down to about ~0.07s

--- app/face_image_processor.py
+++ app/face_image_processor.py
@@ -10,20 +10,31 @@ class ImageProcessor:
     gopigo.set_speed(50)
     gopigo.stop
     self._face_detector = FaceDetector('/home/pi/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
+    self.sizes_calculated = False
+    self.image_height = None
+    self.image_width = None
+    self.segment_width = None
 
   def faces(self, image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return self._face_detector.detect(gray, scaleFactor = 1.1, minNeighbors = 5, minSize = (30, 30))
 
+  def calculate_sizes(self, image):
+    (self.height, self.width) = image.shape[:2]
+    self.segment_width = self.width / 3
+    self.sizes_calculated = True
+
   def process(self, stream):
     start_time = time.time()
     image = cv2.imdecode(np.fromstring(stream.getvalue(), dtype=np.uint8), 1)
 
-    (height, width) = image.shape[:2]
-    segment_width = width / 3
+    if(self.sizes_calculated == False):
+      self.calculate_sizes(image)
 
-    cv2.line(image, (segment_width, 0), (segment_width, height), (255, 0, 0), 1)
-    cv2.line(image, (2 * segment_width, 0), (2 * segment_width, height), (255, 0, 0), 1)
+    segment_width = self.segment_width
+
+    cv2.line(image, (segment_width, 0), (segment_width, self.height), (255, 0, 0), 1)
+    cv2.line(image, (2 * segment_width, 0), (2 * segment_width, self.height), (255, 0, 0), 1)
 
     faceRects = self.faces(image)
     for (x, y, w, h) in faceRects:
