5468a1e84c6099361de3b3d62afca41225f80773
Adam Phillips <aphillips@scholastic.co.uk>
2016-01-23 00:10:57 +0000

    Added initial test for null gopigo

---
+++ app/null_gopigo.py
@@ -0,0 +1,4 @@
+class gopigo:
+  @classmethod
+  def set_speed(self, value):
+    print('Speed set to ' + str(value))
---
+++ test/null_gopigo_test.py
@@ -0,0 +1,6 @@
+from null_gopigo import gopigo
+
+class TestNullGopigo:
+  def test_set_speed(self):
+    # test doesn't fail
+    gopigo.set_speed(50)
